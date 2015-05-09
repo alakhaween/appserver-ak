dbea44fdc8c30a54b7930c2e7b601e4c8d9e9a43
Hazem <hazem@ubuntu.ubuntu-domain>
2015-05-09 14:33:21 +0200

    Initial commit

---
+++ project_addons/ak_core/__openerp__.py
@@ -0,0 +1,25 @@
+#  -*- coding: utf-8 -*-
+
+{
+    "name": "AK Core",
+    "version": "0.1",
+    "author": "Hazem",
+    "category": 'Ak',
+    'complexity': "easy",
+    "description": """
+        AK V1
+    """,
+    'website': "",
+    'images': [],
+    'init_xml': [],
+    "depends": [
+              ],
+    'data': [],
+    'demo_xml': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
+#  vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
